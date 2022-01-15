acc1af58845b268036e1ad700703a1b049073014
Wilson Trovão Beda <wilsontrovaobeda@gmail.com>
2022-01-15 11:17:06 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,28 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
+import { MoviePage } from "types/movie";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // forma errada
-    axios.get(`${BASE_URL}/movies?size=12`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data)
+            const data = response.data as MoviePage
+            setPageNumber(data.number);
         });
+    }, []);
+
+    // forma errada
+    
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
